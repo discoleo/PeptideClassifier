1ce73a6a70b1209330d0e77e48dc9b7b3f6fad55
discoleo <leo.mada@syonic.eu>
2025-07-08 02:36:27 +0300

    Separation between Top 2 topics;

--- R/Modeling.R
+++ R/Modeling.R
@@ -252,3 +252,18 @@ topic.term = function(x, data) {
 	topics(data, 1)[idDoc];
 }
 
+
+### Topic Separation:
+# Diff between Top 2 topics:
+# Note: gamma = proportion of each topic;
+#' @export
+diffTopics = function(x) {
+	t2 = topics(x, 2);
+	nTop = ncol(x@gamma);
+	nDoc = nrow(x@gamma);
+	id1 = nDoc * (t2[1,] - 1) + seq(nDoc);
+	id2 = nDoc * (t2[2,] - 1) + seq(nDoc);
+	gmd = abs(x@gamma[id1] - x@gamma[id2]);
+	return(gmd);
+}
+
