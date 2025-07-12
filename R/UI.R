d33c622096395178d668211011631d9266040242
discoleo <leo.mada@syonic.eu>
2025-07-12 19:05:57 +0300

    UI for n-Grams;

--- R/UI.R
+++ R/UI.R
@@ -85,6 +85,19 @@ panelDTM = function() {
 				value = 0.1, min = 0, max = 2, step = 0.025),
 			actionButton("btnDTM", "Build DTM"),
 			actionButton("btnDTMFilter", "Filter DTM"),
+			# n-Grams:
+			fluidRow(HTML("&nbsp;")),
+			checkboxGroupInput("chkNGrams", "n-Grams",
+				inline = TRUE, width = 360,
+				choices = c("2 Ord" = "2", "2 UnOrd" = "2u",
+					"3 Ord" = "3", "3 UnOrd" = "3u",
+					"4 Ord" = "4", "4 UnOrd" = "4u",
+					"PP Length" = "Len",
+					"3 Gr-Charge" = "ch3.tot", "3 Gr-Charged AA" = "ch3.aa",
+					"4 Gr-Charge" = "ch4.tot", "4 Gr-Charged AA" = "ch4.aa",
+					"5 Gr-Charge" = "ch5.tot", "5 Gr-Charged AA" = "ch5.aa"),
+				selected = c("2", "2u", "3u", "Len", "ch4.tot", "ch4.aa")
+			),
 		),
 		### Main Panel
 		mainPanel(
