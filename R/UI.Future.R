0a0389bab3186e192fad175162340e6038a57c6e
discoleo <leo.mada@syonic.eu>
2025-09-04 18:20:34 +0300

    more comments on Shortcomings of Clustering methods;

--- R/UI.Future.R
+++ R/UI.Future.R
@@ -8,7 +8,7 @@
 ##
 ## URL: https://github.com/discoleo/PeptideClassifier
 ##
-## draft v.0.2c
+## draft v.0.2d
 
 
 ### 
@@ -26,13 +26,32 @@ panelTODO = function() {
 		b("Combined Single & Complete Linkage: "),
 		"combine the single linkage with the complete linkage and use the sum between the 2 distances.",
 		"The maximum distance will counteract the effect of a single pair of very close points.",
-		# Shortcomings:
+		### Analysis:
+		h2("Analysis"),
+		# McQuitty:
+		h3("McQuitty Method:"),
+		"Around half of the leaves join other leaves to form a micro-cluster.",
+		"The remaining leaves are solitary leaves which join a micro- or mini-cluster.",
+		"Only very few solitary leaves join a macro-cluster.",
+		"The micro-clusters merge together to form larger mini-clusters and then macro-clusters.",
+		"There is a good partitioning into medium-sized and large-sized clusters.",
+		### Shortcomings:
 		h2("Shortcomings"),
-		h3("Average Link:"),
-		"The method seems to sequentially add small clusters to a big one.",
+		# Median Linkage:
+		h3("Median Linkage:"),
+		"There are very few micro-clusters (2 leaves), a few mini-clusters and virtually no macro-clusters.",
+		"Almost all leaves join the tree as solitary leaves.",
+		"The solitary leaves join sequentially to a single very large \"branch\".",
+		# Average Linkage:
+		h3("Average Linkage:"),
+		"There are very few macro-clusters.",
+		"Many micro- and mini-clusters join one of the large branches, which grows sequentially to a larger size.",
+		"Most leaves form actually micro-clusters, which merge then to mini-clusters.",
+		"However, the method seems to sequentially add many of these small clusters to a bigger branch.",
 		"It merges very rarely 2 larger clusters! This is easily seen on sub-trees and
 		when pruning a tree: a pruning size of over 100 is needed to get a sparser tree.",
-		"It may be less suited to classify biologically active peptides and infer similar receptor affinities."
+		"It may be less suited to classify biologically active peptides and infer similar receptor affinities.",
+		NBSP()
 	)
 }
 
